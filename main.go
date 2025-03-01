d44c87f2266399565f72ca12857d58080754860b
Kemal <kemalfurkanaraci@gmail.com>
2025-03-02 00:49:30 +0300

    put error show line

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 		db, err = gorm.Open("sqlite3", "invoicer.db")
 	}
 	if err != nil {
+                log.Fatalf("Failed to connect to database: %v", err)
 		panic("failed to connect database")
 	}
 
